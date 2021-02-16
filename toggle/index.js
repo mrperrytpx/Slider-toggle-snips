16c5ce7ff06a264b253c4ea9fc4c00215e40faf8
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-02-16 20:42:35 +0100

    check if checkbox is checked, say it 5 times fast

---
+++ toggle/index.js
@@ -0,0 +1,6 @@
+const checkbox = document.querySelector(".input");
+const text = document.querySelector("#gang");
+
+checkbox.addEventListener("click", () => {
+    text.textContent = checkbox.checked;
+});
